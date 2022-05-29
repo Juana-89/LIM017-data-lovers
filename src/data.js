18b5b203b2e263e902125ecd191972b5266628ea
Juana-89 <juana.lc@hotmail.es>
2022-05-29 13:07:27 -0500

    Do responsive model part:1

--- src/data.js
+++ src/data.js
@@ -3,22 +3,22 @@ export const sortData = (data,sortOrder) => {
     if (sortOrder === "A-Z") {
       data.sort((a,b) => {
         if (a.title > b.title) {
-            return 1;
+        return 1;
         }else if (a.title < b.title) {
-            return -1;
+        return -1;
         }
         return 0;
     })
   }
   else {
-  data.sort((a, b) => {
-    if (a.title > b.title) {
+    data.sort((a, b) => {
+      if (a.title > b.title) {
       return -1;
-    }else if (a.title < b.title) {
+      }else if (a.title < b.title) {
       return 1;
-    }
-    return 0;
-  })
+      }
+      return 0;
+    })
 }
 return data;
 };
@@ -28,9 +28,9 @@ export const sortMovieRanking =(data, sortOrder) => {
     if (sortOrder === "Menos rankeadas") {
       data.sort((c,d) => { 
         if (parseInt(c.rt_score) > parseInt(d.rt_score)) {
-              return 1;
+        return 1;
         } else if (parseInt(c.rt_score) < parseInt(d.rt_score)) {
-              return -1;
+        return -1;
         }
         return 0;
         })
@@ -38,9 +38,9 @@ export const sortMovieRanking =(data, sortOrder) => {
 else {
       data.sort((c,d)=>{
         if (parseInt(c.rt_score) > parseInt(d.rt_score)) {
-            return -1;
+        return -1;
         } if (parseInt(c.rt_score) < parseInt(d.rt_score)) {
-            return 1;
+        return 1;
         }
         return 0;
 })
@@ -51,21 +51,21 @@ return data;
 /*objeto filtra por año de publicación*/
 export const filterYearPublished = (films ,release_date) => {
    const filterMovie = films.filter ((film) => {
-  if (release_date === film.release_date) {
+    if (release_date === film.release_date) {
     return true;
-  } else {
-      return false;
-  }});
+    } else {
+    return false;
+    }});
   return filterMovie;
 };
 
 /*objeto filtra por productor*/
 export const filterMoviexProducer = (films, nameProducer) => {
   const filterNameProducer = films.filter ((film) => {
-  if (nameProducer === film.producer) {
-      return true;
+    if (nameProducer === film.producer) {
+    return true;
   } else {
-      return false;
+    return false;
   }});
   return filterNameProducer;
 };
